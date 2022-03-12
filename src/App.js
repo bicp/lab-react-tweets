51873f8c5fa5c61457b09d0b07a880aca8ebe187
Bianca Pimenta <bi.cp1802@gmail.com>
2022-03-12 09:12:40 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-import "./App.css";
-import Tweet from "./components/Tweet";
+// Imports
 
-const tweetsArray = [
-  {
-    user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
-    },
-    timestamp: "1h ago",
-    message:
-      "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
-  },
-  {
-    user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
-    },
-    timestamp: "2h ago",
-    message:
-      "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
-  },
-  {
-    user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
-    },
-    timestamp: "3h ago",
-    message:
-      "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
-  },
-];
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Action from "./Action";
 
-function App() {
+// Function
+
+function Tweet(props) {
   return (
-    <div className="App">
-      <Tweet />
+    <div className="tweet">
+      <ProfileImage image={props.tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <User userdata={props.tweet.user} />
+          <Timestamp time={props.tweet.timestamp} />
+        </div>
+
+        <Message message={props.tweet.message} />
+
+        <Action />
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
     </div>
   );
 }
 
-export default App;
+export default Tweet;
